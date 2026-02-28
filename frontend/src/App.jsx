1be287760b6dc9ccdd3ced8f5b85bacb9d5c92cc
Md Irfan <rn555666777@gmail.com>
2026-02-28 22:32:29 +0530

    Initial portfolio setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contacts/Contact";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/About/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <div>About Componrnt</div>;
+}
+
+export default About;
