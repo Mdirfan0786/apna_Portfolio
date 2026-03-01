7ac912728ec0fd90c224eff0f7fbc6872cf44c9a
Md Irfan <rn555666777@gmail.com>
2026-03-01 16:23:02 +0530

    feat: add responsive navbar with routing

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import styles from "./Navbar.module.css";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className={styles.Nav_Container}>
+        {/* Nav Logo */}
+        <div className={styles.Nav_Logo}>
+          <img src="/portfolio_logo.png" alt="portfolio_logo" />
+        </div>
+
+        {/* Nav List */}
+        <div className={styles.Nav_list}>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            About
+          </NavLink>
+
+          <NavLink
+            to="/projects"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Projects
+          </NavLink>
+
+          <NavLink
+            to="/skills"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Skills
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Contact Me
+          </NavLink>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
